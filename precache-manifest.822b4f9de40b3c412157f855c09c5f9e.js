9d8827cae8e27ecfdcc3ec9c62f55c6e32a8ea4c
MonicaBartha <barthamonica@gmail.com>
2020-10-11 21:02:56 +0200

    Updates

---
+++ precache-manifest.822b4f9de40b3c412157f855c09c5f9e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d60c5213a5daeab2b81f6a3463a0b6d",
+    "url": "/aquest-challenge/index.html"
+  },
+  {
+    "revision": "4d86c67065707b5cb2b7",
+    "url": "/aquest-challenge/static/css/main.709e2a1a.chunk.css"
+  },
+  {
+    "revision": "5edeb9df9d4cf46eefdc",
+    "url": "/aquest-challenge/static/js/2.b9749d1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aquest-challenge/static/js/2.b9749d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d86c67065707b5cb2b7",
+    "url": "/aquest-challenge/static/js/main.f429a6cc.chunk.js"
+  },
+  {
+    "revision": "16220cb6c3d5536e861e",
+    "url": "/aquest-challenge/static/js/runtime-main.35e81936.js"
+  },
+  {
+    "revision": "198a9fc6a5ac04b6b8afa8a00b3b9b49",
+    "url": "/aquest-challenge/static/media/Tungsten-Bold.198a9fc6.otf"
+  },
+  {
+    "revision": "8e2353fae1c679997470ddf270a2dab9",
+    "url": "/aquest-challenge/static/media/Tungsten-Semibold.8e2353fa.otf"
+  },
+  {
+    "revision": "afc3c54a6d08b28c2cadee667d5c1cd4",
+    "url": "/aquest-challenge/static/media/kevin-laminto.afc3c54a.jpg"
+  },
+  {
+    "revision": "220e0a00b436ded8d0df0c298670df53",
+    "url": "/aquest-challenge/static/media/nicole-honeywill.220e0a00.jpg"
+  },
+  {
+    "revision": "eac6ccf14cd939cdae9e176b05debbdd",
+    "url": "/aquest-challenge/static/media/oladimeji-odunsi.eac6ccf1.jpg"
+  },
+  {
+    "revision": "1c090136152c02f8cd1ffdb78b96844a",
+    "url": "/aquest-challenge/static/media/pamela-lima.1c090136.jpg"
+  }
+]);
